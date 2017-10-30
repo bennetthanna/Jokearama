d8f1114f0a8db34374c737019f3e1e13b47719d6
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-30 11:11:25 -0700

    Start list fragment set up

--- app/src/main/java/com/hannabennett/jokearama/MainActivity.java
+++ app/src/main/java/com/hannabennett/jokearama/MainActivity.java
@@ -1,13 +1,13 @@
 package com.hannabennett.jokearama;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+    protected Fragment createFragment() {
+        return new JokeListFragment();
     }
 }
