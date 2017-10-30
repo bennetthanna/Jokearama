3aeb30b72a1451dd89330d387518a859da1d8db7
Hanna Bennett <bennetthanna22@gmail.com>
2017-10-30 11:19:21 -0700

    Create adapter and generate 3 method overrides Cursor over extends -> Alt+Enter -> Implement methods

--- app/src/main/java/com/hannabennett/jokearama/JokeListFragment.java
+++ app/src/main/java/com/hannabennett/jokearama/JokeListFragment.java
@@ -1,12 +1,15 @@
 package com.hannabennett.jokearama;
 
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * Created by HannaBennett on 10/30/17.
@@ -29,6 +32,29 @@ public class JokeListFragment extends Fragment {
     private class JokeHolder extends RecyclerView.ViewHolder {
         public JokeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_joke, parent, false));
+        }
+    }
+
+    private class JokeAdapter extends RecyclerView.Adapter<JokeHolder> {
+        private List<Joke> mJokes;
+
+        public JokeAdapter(List<Joke> jokes) {
+            mJokes = jokes;
+        }
+
+        @Override
+        public JokeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(JokeHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
         }
     }
 }
